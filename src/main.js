30caf41fdbd5acf53570495bf4f8df7d5e7ca04a
FloppyAEP <the22thoctober@gmail.com>
2021-08-13 12:53:51 +0300

    remove button add

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ const addTaskBtn = document.querySelector('.content__addTaskButton')
 const taskInput = document.querySelector('.content__input')
 
 class Task{
-    constructor(taskValue){
+    constructor(taskValue, tasksContainer){
         this.taskValue = taskValue
+        this.tasksContainer = tasksContainer
         this.disableTask = ()=>{
             this.taskValue.classList.toggle('content_task-disable')
+        }
+        this.removeTask = ()=>{
+            this.tasksContainer.remove()
         }
     }
 }
@@ -14,18 +18,27 @@ class Task{
 const addTask = ()=>{
     if(taskInput.value != ''){
         let taskWrapper = document.createElement('div')
+        let taskMain = document.createElement('div')
         let taskText = document.createElement('p')
         let taskCheckbox = document.createElement('input')
-        let task = new Task(taskText)
-    
+        let removeTask = document.createElement('button')
+        let task = new Task(taskText, taskWrapper)
+        
+        taskMain.classList.add('content__taskMain')
+        removeTask.classList.add('btn')
+        removeTask.classList.add('btn-primary')
         taskWrapper.classList.add('content__taskWrapper')
         taskText.classList.add('content__task')
         taskCheckbox.type = 'checkbox'
         taskText.innerHTML = taskInput.value
-        taskWrapper.appendChild(taskCheckbox)
-        taskWrapper.appendChild(taskText)
+        removeTask.innerHTML = 'Remove task'
+        taskMain.appendChild(taskCheckbox)
+        taskMain.appendChild(taskText)
+        taskWrapper.appendChild(taskMain)
+        taskWrapper.appendChild(removeTask)
         tasksContainer.appendChild(taskWrapper)
         taskCheckbox.onchange = task.disableTask
+        removeTask.onclick = task.removeTask
         taskInput.value = ''        
     }
     else{
